d014ae2c586a3e72ebd2b5b04ada45183de654cc
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2021-07-22 17:41:54 -0400

    Replace - and = with braces on the layer keys

--- keyboards/planck/keymaps/bunner/keymap.c
+++ keyboards/planck/keymaps/bunner/keymap.c
@@ -51,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Ent/Sh|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  |   \  |Lwr/- |    Space    |Rse/= |   /  | Left | Down |Right |
+ * | Ctrl | Alt  | GUI  |   \  |Lwr/[ |    Space    |Rse/] |   /  | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     LT(3, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LT(7, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, RSFT_T(KC_ENT),
-    KC_LCTL, KC_LALT, KC_LGUI, KC_BSLS, LT(3, KC_MINS),   KC_SPC,  KC_SPC,  LT(4, KC_EQL),   KC_SLSH, KC_LEFT, KC_DOWN,   KC_RGHT
+    KC_LCTL, KC_LALT, KC_LGUI, KC_BSLS, LT(3, KC_LBRC),   KC_SPC,  KC_SPC,  LT(4, KC_RBRC),   KC_SLSH, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 /* Colemak
@@ -171,9 +171,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 /* ESC (Hold Esc)
- *                      v------------------------RGB CONTROL--------------------v
  * ,-----------------------------------------------------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  Del |
+ * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | F11  | F12  |      |      |      |      |      |      |      |      |   \  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -183,8 +182,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ESC] = LAYOUT_planck_grid(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10, KC_DEL ,
-    _______, KC_F11,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10, KC_DEL ,
+    _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
     _______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, _______, KC_HOME, KC_PGDN, KC_END
 ),
