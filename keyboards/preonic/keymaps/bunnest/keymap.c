94f0f1012625b6bf180895fe3abd9c2dcbe69912
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2022-01-01 19:27:16 -0500

    Make bunnest preonic layout more like the planck one

--- keyboards/preonic/keymaps/bunnest/keymap.c
+++ keyboards/preonic/keymaps/bunnest/keymap.c
@@ -21,7 +21,8 @@
 
 enum preonic_layers {
   _QWERTY,
-  _ESC
+  _ESC,
+  _TAB
 };
 
 enum preonic_keycodes {
@@ -37,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Tab/2|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc/1|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -47,34 +48,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  LT(1, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, RSFT_T(KC_ENT),
-  KC_LCTL, KC_LALT, KC_LGUI, KC_BSLS, KC_LBRC,   KC_SPC,  KC_SPC,  KC_RBRC,   KC_SLSH, KC_LEFT, KC_DOWN,   KC_RGHT
+  KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  LT(2, KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  LT(1, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_ENT),
+  KC_LCTL,       KC_LALT, KC_LGUI, KC_BSLS, KC_LBRC, KC_SPC,  KC_SPC,  KC_RBRC, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Esc
  * ,-----------------------------------------------------------------------------------.
- * | Reset|  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |   =  |
+ * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |   =  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Debug|  F11 |  F12 |      |      |      |      |      |      |      |      |  Del |
+ * | Tab  |  F11 |  F12 |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | xxxx |      |Aud cy|Aud on|AudOff|      |      |Mac2Rc|MacStp|Mac2Pl|      | Vol+ |
+ * | xxxx |      |      |RGB_MD| Sat+ | Brt+ | Hue+ |Mac2Rc|MacStp|Mac2Pl|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|Mac1Rc|MacStp|Mac1Pl| PgUp | Vol- |
+ * |      | Debug| Reset| RGB  | Sat- | Brt- | Hue- |Mac1Rc|MacStp|Mac1Pl| PgUp |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |SC_Mc | SC_Ch| SC_M |Brite | Prev |    Play     | Next | Mute | Home | PgDn | End  |
+ * |      |      |      |Brite | Prev |    Play     | Next |      | Home | PgDn | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_ESC] = LAYOUT_preonic_grid(
-  RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
-  DEBUG,   KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
-  _______, _______,  MU_MOD,  AU_ON,   AU_OFF,  _______, _______, DM_REC2, DM_RSTP, DM_PLY2, _______, KC_VOLU,
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  DM_REC1, DM_RSTP, DM_PLY1, KC_PGUP, KC_VOLD,
-  LCTL(LSG(KC_4)), RCS(KC_F5), LSG(KC_4), BACKLIT, KC_MPRV, KC_MPLY, KC_MPLY, KC_MNXT, KC_MUTE, KC_HOME, KC_PGDN, KC_END
-)
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
+  KC_TAB,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+  _______, _______, _______, RGB_MOD, RGB_SAI, RGB_VAI, RGB_HUI, DM_REC2, DM_RSTP, DM_PLY2, _______, _______,
+  _______, DEBUG,   RESET,   RGB_TOG, RGB_SAD, RGB_VAD, RGB_HUD, DM_REC1, DM_RSTP, DM_PLY1, KC_PGUP, _______,
+  _______, _______, _______, BACKLIT, KC_MPRV, KC_MPLY, KC_MPLY, KC_MNXT, _______, KC_HOME, KC_PGDN, KC_END
+),
 
+/* Tab
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |   =  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | xxxx |  F11 |  F12 |      |      |      |      |      |      |      |      | Del  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |      |      |      |      |      |      |      |      | MBck | MClk | MFwd |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | Debug| Reset|      |      |      |      |MusTog|MusMod| LClk | MsUp | RClk |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      | Vol- |    Mute     | Vol+ |      | MsLt | MsDn | MsRt |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_TAB] = LAYOUT_preonic_grid(
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,      KC_F10,     KC_EQL,
+  _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,    _______,    KC_DEL,
+  KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_BTN4, KC_MS_BTN3, KC_MS_BTN5,
+  _______, DEBUG,   RESET,   _______, _______, _______, _______, MU_TOG,  MU_MOD,  KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2,
+  _______, _______, _______, _______, KC_VOLD, KC_MUTE, KC_MUTE, KC_VOLU, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT
+),
 
 };
 
--- keyboards/preonic/keymaps/bunnest/readme.md
+++ keyboards/preonic/keymaps/bunnest/readme.md
@@ -6,7 +6,7 @@
 ,-----------------------------------------------------------------------------------.
 |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  -   |
 |------+------+------+------+------+------+------+------+------+------+------+------|
-| Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+| Tab/2|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
 |------+------+------+------+------+-------------+------+------+------+------+------|
 | Esc/1|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
 |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -19,5 +19,9 @@
 ## Features
 
 -   Inverted T arrow keys (had to move `/` elsewhere but it's worth it)
--   Only two layers! ESC layer accessible by holding escape
--   QWERTY layer contains every 60% key _except_ for `=` (and caps lock I guess but WHO USES THAT?)
+-   Three layers! You could fit everything in two but the extra layer makes it more intuitive and allows for a matching planck layout
+    - `QWERTY` - contains every 60% key _except_ for `=` (and caps lock I guess but WHO USES THAT?)
+    - `ESC` - accessed by holding escape, contains keys commonly used when typing (like `=` and `delete`) plus macros and media keys
+    - `TAB` - accessed by holding tab, contains less commonly used "fun" keys like music mode, volume controls, and mouse keys
+-   Enter acts like a right shift when held
+-   Escape, tab, and enter are [mod-tap keys](https://docs.qmk.fm/#/mod_tap) but you can make them act normally if you hold escape (or tab)
